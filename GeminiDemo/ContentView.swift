dfeaec1e26ef972bbda668d45d59d423f95e6ace
evan <evan27105615@yahoo.com.tw>
2024-08-12 23:46:18 +0800

    code update

--- GeminiDemo/ContentView.swift
+++ GeminiDemo/ContentView.swift
@@ -6,18 +6,81 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    
+    
+    @State private var textInput = ""
+    @State private var response: LocalizedStringKey = "Hello! How can I help you today?"
+
+    @State private var isThinking = false
+
+    let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    
+    
+    
+    func sendMessage() {
+        response = "Thinking..."
+        
+        withAnimation(.easeInOut(duration: 0.6).repeatForever(autoreverses: true)) {
+            isThinking.toggle()
         }
-        .padding()
+        
+        Task {
+            do {
+                let generatedResponse = try await model.generateContent(textInput)
+                
+                guard let text = generatedResponse.text else  {
+                    textInput = "Sorry, Gemini got some problems.\nPlease try again later."
+                    return
+                }
+                
+                textInput = ""
+                response = LocalizedStringKey(text)
+                
+                isThinking.toggle()
+            } catch {
+                response = "Something went wrong!\n\(error.localizedDescription)"
+            }
+        }
+    }
+
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            ScrollView {
+                VStack {
+                    Text(response)
+                        .font(.system(.title, design: .rounded, weight: .medium))
+                        .opacity(isThinking ? 0.2 : 1.0)
+                }
+            }
+            .contentMargins(.horizontal, 15, for: .scrollContent)
+            
+            Spacer()
+            
+            HStack {
+                
+                TextField("Type your message here", text: $textInput)
+                    .textFieldStyle(.plain)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .onSubmit {
+                        sendMessage()
+                    }
+                
+            }
+            .padding(.horizontal)
+        }
     }
 }
+
 
 #Preview {
     ContentView()
