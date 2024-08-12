5f0497a1b91d3d65aab4053d9fcebe0527b96d6a
evan <evan27105615@yahoo.com.tw>
2024-08-12 19:33:46 +0800

    Initial Commit

---
+++ GeminiDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiDemo
+//
+//  Created by Evan Lu on 2024/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiDemo/GeminiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiDemoApp.swift
+//  GeminiDemo
+//
+//  Created by Evan Lu on 2024/8/12.
+//
+
+import SwiftUI
+
+@main
+struct GeminiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
